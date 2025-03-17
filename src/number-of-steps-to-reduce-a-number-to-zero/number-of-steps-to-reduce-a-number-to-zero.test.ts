4cf70752f3b3e5812a2c68580d9fea7fa9fa513d
Ilia Evseev <ilia.evseev@icloud.com>
2025-03-18 00:43:16 +0100

    Added challenge Number of Steps to Reduce a Number to Zero

---
+++ src/number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.test.ts
@@ -0,0 +1,15 @@
+import { expect, test } from "@jest/globals";
+
+import numberOfStepsToReduceANumberToZero from "./number-of-steps-to-reduce-a-number-to-zero";
+
+test("num = 14", () => {
+  expect(numberOfStepsToReduceANumberToZero(14)).toBe(6);
+});
+
+test("num = 8", () => {
+  expect(numberOfStepsToReduceANumberToZero(8)).toBe(4);
+});
+
+test("num = 123", () => {
+  expect(numberOfStepsToReduceANumberToZero(123)).toBe(12);
+});
---
+++ src/number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.ts
@@ -0,0 +1,11 @@
+export default function main(num: number): number {
+  let steps = 0;
+
+  while (num !== 0) {
+    const even = num % 2 === 0;
+    num = even ? num / 2 : num - 1;
+    steps++;
+  }
+
+  return steps;
+}
